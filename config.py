0a0c968e5dd62909eb212a656d70c01c8602b03f
michalmoninec <57136223+michalmoninec@users.noreply.github.com>
2024-12-05 19:21:43 +0100

    env update

--- config.py
+++ config.py
@@ -1,11 +1,17 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
 class Config:
-    SECRET_KEY = "aasdqweoriuqpwoeiu"
+    SECRET_KEY = os.getenv("APP_SECRET_KEY")
     SQLALCHEMY_DATABASE_URI = "sqlite:///map.sqlite3"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
 class TestConfig:
-    SECRET_KEY = "anotherkey"
+    SECRET_KEY = os.getenv("CONFIG_SECRET_KEY")
     SQLALCHEMY_DATABASE_URI = "sqlite:///:memory:"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     TESTING = True
