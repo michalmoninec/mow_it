059a9e471b318fbf4d78c5b71836a8292e59658f
michalmoninec <57136223+michalmoninec@users.noreply.github.com>
2024-12-05 21:31:27 +0100

    enum path change

--- app/enums.py
+++ app/enums.py
@@ -1,9 +1,13 @@
 import json
+import os
+
 from enum import Enum
+
+file_path = os.path.join(os.path.dirname(__file__), "config/status_enum.json")
 
 
 def load_status_enum() -> Enum:
-    with open("config/status_enum.json", "r") as f:
+    with open(file_path, "r") as f:
         enum_data = json.load(f)
 
         Status = Enum("Status", enum_data["Status"])
